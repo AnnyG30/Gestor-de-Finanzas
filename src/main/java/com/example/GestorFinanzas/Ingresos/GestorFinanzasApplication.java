710392840e79b9999f3424c8bbba21a913ebc7d3
AnnyG30 <annygalvez30@outlook.es>
2025-11-09 14:49:02 +0100

    eliminando paquetes vacios

---
+++ src/main/java/com/example/GestorFinanzas/Ingresos/GestorFinanzasApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestorFinanzas.Ingresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinanzasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorFinanzasApplication.class, args);
+    }
+
+}
