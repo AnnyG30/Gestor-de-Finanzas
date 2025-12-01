e8692728d32881547146703b17003e8333cce0b4
AnnyG30 <annygalvez30@outlook.es>
2025-12-01 10:46:04 +0100

    espero que ahora funcione

--- src/main/java/com/example/GestorFinanzas/Shared/Files/Infra/Config/StorageProperties.java
+++ src/main/java/com/example/GestorFinanzas/Shared/Files/Infra/Config/StorageProperties.java
@@ -3,14 +3,14 @@ package com.example.GestorFinanzas.Shared.Files.Infra.Config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-
+@Component
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
     /**
      * Folder location for storing files
      */
-    private String location = "upload";
+    private String location = "uploads";
 
     public String getLocation() {
         return location;
